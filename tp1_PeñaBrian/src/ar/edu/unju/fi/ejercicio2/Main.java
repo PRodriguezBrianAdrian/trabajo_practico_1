6d4beb67e21096384d2d6e1e823cd1c9415b228a
PRodriguezBrianAdrian <brianrodriguezlprc@gmail.com>
2024-04-06 00:36:13 -0300

    Ejercicio 2 corregido

--- tp1_PeñaBrian/src/ar/edu/unju/fi/ejercicio2/Main.java
+++ tp1_PeñaBrian/src/ar/edu/unju/fi/ejercicio2/Main.java
@@ -10,12 +10,12 @@ public class Main {
 		String numTel = "3885088987";
 		double calCos = 0.87758256189;
 
-		System.out.println(nombrePais);
-		System.out.println(edad);
-		System.out.println(alturaEdif);
-		System.out.println(precioProduc);
-		System.out.println(numTel);
-		System.out.println(calCos);
+		System.out.println("Pais: " + nombrePais);
+		System.out.println("Edad: " + edad);
+		System.out.println("Altura de un edificio: " + alturaEdif);
+		System.out.println("Precio de un Productor: " + precioProduc);
+		System.out.println("Numero Telefonico: " + numTel);
+		System.out.println("Calculo del coseno: " + calCos);
 	}
 
 }

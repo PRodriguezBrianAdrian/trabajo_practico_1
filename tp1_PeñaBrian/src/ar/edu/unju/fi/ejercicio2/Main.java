78f47a789437bcd3da33af191cf13fc42ab5dbbc
PRodriguezBrianAdrian <brianrodriguezlprc@gmail.com>
2024-04-06 00:29:38 -0300

    Ejercicio 1 y 2 finalizados

---
+++ tp1_PeñaBrian/src/ar/edu/unju/fi/ejercicio2/Main.java
@@ -0,0 +1,21 @@
+package ar.edu.unju.fi.ejercicio2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		String nombrePais = "Argentina";
+		int edad = 27;
+		double alturaEdif = 85.0;
+		double precioProduc = 1339.99;
+		String numTel = "3885088987";
+		double calCos = 0.87758256189;
+
+		System.out.println(nombrePais);
+		System.out.println(edad);
+		System.out.println(alturaEdif);
+		System.out.println(precioProduc);
+		System.out.println(numTel);
+		System.out.println(calCos);
+	}
+
+}
